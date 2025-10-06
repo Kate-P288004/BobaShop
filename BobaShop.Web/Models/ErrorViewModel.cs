98ea55821e0d06e10edb35d888daaf783cabb24d
Kate <kate.odabas@gmail.com>
2025-10-06 13:57:31 +0800

    Add project files.

---
+++ BobaShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BobaShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
