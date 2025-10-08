e3d6c8cf155aa7b8ef607af82588817c290799df
Kate <kate.odabas@gmail.com>
2025-10-08 16:28:13 +0800

    add Topping DTO classes for create and update operations

---
+++ BobaShop.Api/Dtos/ToppingDtos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BobaShop.Api.Dtos
+{
+    public class ToppingCreateDto
+    {
+        [Required, MinLength(2)]
+        public string Name { get; set; } = default!;
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; } = 0.80m;
+
+        public bool IsActive { get; set; } = true;
+    }
+
+    public class ToppingUpdateDto : ToppingCreateDto { }
+}
