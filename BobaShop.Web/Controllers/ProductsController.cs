6587e393a9049927f7718b698db520e4f8a2e439
Kate <kate.odabas@gmail.com>
2025-10-13 16:49:40 +0800

    Created Models/ProductViewModel.cs for product data structure

---
+++ BobaShop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BobaShop.Web.Services;
+
+namespace BobaShop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApiService _api;
+        public ProductsController(ApiService api) => _api = api;
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _api.GetProductsAsync();
+            return View(products);
+        }
+    }
+}
