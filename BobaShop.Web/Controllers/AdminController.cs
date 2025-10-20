0dcf1438ba04237a79955cf9a60181a075a51af6
Kate <kate.odabas@gmail.com>
2025-10-20 16:23:23 +0800

    ecure pages with [Authorize]; add Admin-only page

---
+++ BobaShop.Web/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// File: Controllers/AdminController.cs
+// Student: Kate Odabas (P288004)
+// Purpose: Simple Admin-only page for verification/screenshots
+// ---------------------------------------------------------------
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BobaShop.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
